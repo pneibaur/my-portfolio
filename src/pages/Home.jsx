3db8c5e150a4e69840a7a7439a34ef148f0a5bd4
Phil Neibaur <pneibaur@Phils-MacBook-Pro.local>
2022-09-27 21:54:46 -0600

    adds home banner

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,6 +5,7 @@ const Home = () => {
     return (
         <div className="main">
             <h1 className='title'>Welcome to my super awesome portfolio!</h1>
+            <img className='banner' src="https://i.imgur.com/qkJU5nh.jpg" alt="Utah Tech" />
             <h3 className='subtitle'>Checkout my projects!</h3>
             <button><Link className='link' to="/projects">MY STUFF</Link></button>
         </div>
